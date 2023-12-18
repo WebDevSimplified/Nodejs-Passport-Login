089e8a1027d83956aa21329d0e7af60a007ba934
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2023-12-19 00:10:16 +0300

    run the aplication

--- server.js
+++ server.js
@@ -22,11 +22,13 @@ const users = []
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(flash())
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false
 }))
+
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
